914d04b0f6bfde86b50e3c2f748987bd59fef98f
wangyu201393 <51938726+wangyu201393@users.noreply.github.com>
2022-03-16 16:53:38 +0800

    init 1653

--- index.js
+++ index.js
@@ -27,12 +27,21 @@ const client = axios.default
 app.post('/api/message', async (req, res) => {
     const headers = req.headers
     const token = headers['x-wx-cloudbase-access-token']
-    const weixinAPI = `https://api.weixin.qq.com/cgi-bin/message/custom/send?cloudbase_access_token=${token}`
+    const weixinAPI = `https://api.weixin.qq.com/cgi-bin/message/subscribe/send`
     const payload = {
         touser: headers['x-wx-openid'],
-        msgtype: 'text',
-        text: {
-            content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
+        template_id: 'd7XQzPj4rx9b3McEVbuo_C7TPXuL6B5h',
+        page: 'pages/albumMaker/assetsManager/assetsManager',
+        data: {
+            name1: {
+                value: '丁真'
+            },
+            thing3: {
+                value: '义眼'
+            },
+            phrase1: {
+                value: '真'
+            },
         }
     }
     // dispatch to wx server
