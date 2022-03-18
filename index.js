b1dbffe7bba5e1b06a515f294b4016230f23a33e
wangyu201393 <51938726+wangyu201393@users.noreply.github.com>
2022-03-18 18:15:09 +0800

    getQR 1815

--- index.js
+++ index.js
@@ -74,16 +74,16 @@ console.log(`Running on http://${HOST}:${PORT}`)
 // 获取分享二维码
 app.post("/api/getQR", async (req, res) => {
     const headers = req.headers
-    const token = headers['x-wx-cloudbase-access-token']
+    // const token = headers['x-wx-cloudbase-access-token']
     const payload = {
-        cloudbase_access_token: token,
+        // cloudbase_access_token: token,
         page: req.body.path,
         scene: req.body.query
     }
     return new Promise((resolve, reject) => {
         request({
             method: 'POST',
-            url: `https://api.weixin.qq.com/wxa/getwxacodeunlimit?cloudbase_access_token=${token}`,
+            url: `https://api.weixin.qq.com/wxa/getwxacodeunlimit`,
             body: JSON.stringify(payload)
         }, function (error, response) {
             console.log('接口返回内容', response.body)
